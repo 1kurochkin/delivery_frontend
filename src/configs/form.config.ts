df208d00a93da429e397b9b82d2cd8b420779f59
flexwappa <flexwappa@gmail.com>
2022-08-05 04:42:23 -0400

    initialize form.config

---
+++ src/configs/form.config.ts
@@ -0,0 +1,12 @@
+export default {
+    phone: {
+        field: 'phone',
+        text: 'Please write your phone number correct form like 19168918139',
+        regexp: ''
+    },
+    code: {
+        field: 'code',
+        text: 'Please write your verification code!',
+        regexp: ''
+    }
+}
