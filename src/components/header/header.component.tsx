a3a43423a0d1fc4ad103c4edc05a1dda19ac11b5
flexwappa <flexwappa@gmail.com>
2022-08-10 03:10:57 -0400

    update header component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,35 +1,68 @@
-import { Button } from 'antd';
+import {Button, Col, Row} from 'antd';
 import React from 'react';
 import {Link} from "react-router-dom";
 import {ROUTES} from "../../configs/constants";
 import './header.component.scss';
+import {useAppSelector} from "../../hooks/useAppSelector";
+import Title from "antd/lib/typography/Title";
+import {UserRoleEnum} from "../../store/reducers/backend/backend.api.types";
 
 
 export function Header() {
+    const auth = useAppSelector(({app}) => app.auth)
+    const userRole = useAppSelector(({settings}) => settings.role)
+    const navigationConfig = {
+        authTrue: {
+            courier: [
+                {link: ROUTES.CREATE_ORDER, label: "Orders"},
+                {link: ROUTES.SETTINGS_PAGE, label: "Settings"},
+            ],
+            customer: [
+                {link: ROUTES.CREATE_ORDER, label: "Create Order"},
+                {link: ROUTES.CREATE_ORDER, label: "My Orders"},
+                {link: ROUTES.SETTINGS_PAGE, label: "Settings"},
+            ]
+        },
+        authFalse: [
+            {link: ROUTES.BECOME_COURIER, label: "Become a courier"},
+            {link: ROUTES.CREATE_ORDER, label: "Create order"},
+            {link: ROUTES.LOGIN_PAGE, label: "Login and Sign up"},
+        ]
+    }
+
+    const getViewByNavigationConfig = (navConfig: Array<{link: string, label: string}>) => {
+        return navConfig.map(({link, label}, i) =>
+            <Col offset={i > 0 ? 1 : 0} span={7}>
+                <Button>
+                    <Link to={link}>{label}</Link>
+                </Button>
+            </Col>
+        );
+    }
+
+    const getNavigationView = () => {
+        let result;
+        if(auth) {
+            // @ts-ignore
+            result = getViewByNavigationConfig(navigationConfig?.authTrue[userRole]);
+        } else {
+            result = getViewByNavigationConfig(navigationConfig?.authFalse)
+        }
+        return <>{result}</>;
+    }
+
     return (
         <div className="header">
-            <div className={'header__container'}>
-                <div className={'header__logo'}>
-                    {/*<img className={'header__logo-img'} src={logo} alt={'logo'}/>*/}
-                    <span className={'title'}>Bringa.me</span>
-                    <div className={'header__logo-location'}>
-                        {/*<EnvironmentFilled  />*/}
-                        {/*<span className={'header__logo-location-span'}>NYC</span>*/}
-                    </div>
-                </div>
-
-                <div className={'header__nav'}>
-                    <Button className={'button-pink'}>
-                        <Link to={ROUTES.BECOME_COURIER}>Become a courier</Link>
-                    </Button>
-                    <Button className={'header__nav-link button-pink'}>
-                        <Link  to={ROUTES.CREATE_ORDER}>Make order</Link>
-                    </Button>
-                    <Button className={'header__nav-link button-pink'}>
-                        <Link to={ROUTES.LOGIN_PAGE}>Login and Sign up</Link>
-                    </Button>
-                </div>
-            </div>
+            <Row justify={'space-around'}>
+                <Col span={3}>
+                    <Link to={ROUTES.MAIN_PAGE}>
+                        <Title level={2}>Bringa.me</Title>
+                    </Link>
+                </Col>
+                <Col style={{display: 'flex'}} span={14}>
+                    {getNavigationView()}
+                </Col>
+            </Row>
         </div>
-    );
+);
 }
