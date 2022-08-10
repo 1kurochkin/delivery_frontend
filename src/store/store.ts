174c210f54cb0aba133bd55f8f70f538e80d50fc
flexwappa <flexwappa@gmail.com>
2022-08-10 02:59:29 -0400

    create redux store

---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {appReducer} from "./reducers/app/app.slice";
+import {setupListeners} from "@reduxjs/toolkit/query";
+import {backendApi} from "./reducers/backend/backend.api";
+import {settingsReducer} from "./reducers/settings/settings.slice";
+
+export const store = configureStore({
+    reducer: {
+        [backendApi.reducerPath]: backendApi.reducer,
+        app: appReducer,
+        settings: settingsReducer,
+    }
+});
+
+setupListeners(store.dispatch);
+
+export type RootStateType = ReturnType<typeof store.getState>
