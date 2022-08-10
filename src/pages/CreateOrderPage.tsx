6eaee0a901ee1d3cafdf5f8379d9cc08dd3b15c4
flexwappa <flexwappa@gmail.com>
2022-08-10 03:11:13 -0400

    remove unused

--- src/pages/CreateOrderPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function CreateOrderPage() {
-    return (
-        <div>CreateOrderPage</div>
-    );
-};
--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {backendReducer} from './reducers/backend/backend.reducer'
-
-export const store = configureStore({
-    reducer: {
-        [backendReducer.reducerPath]: backendReducer.reducer
-    }
-})
