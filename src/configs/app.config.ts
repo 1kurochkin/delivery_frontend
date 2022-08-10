0b3653ecb4f50f47a7c92a4716a12e26474b359b
flexwappa <flexwappa@gmail.com>
2022-08-10 02:55:03 -0400

    update appConfig

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -5,5 +5,15 @@ export default {
         prod: {url: 'https://bringa.me'},
         local: {url: 'http://localhost:5000'}
 
+    },
+    google: {
+        maps: {
+            apiKey: 'AIzaSyCjC6fjRhFxNS__wDGytq-xINSYOwt-EwQ'
+        },
+        captcha: {
+            id: '6Lf2MVAhAAAAAIgEejk2ohSrK48oCWTEc4rJEQ3i',
+            siteKey: '6LcoKlEhAAAAAHr4Chv37XysguCrsVt0UEIkmiux',
+            secretkey: '6LcoKlEhAAAAAAGJ8lJROleORRUOm-HSfzGjBnB-'
+        }
     }
 }
