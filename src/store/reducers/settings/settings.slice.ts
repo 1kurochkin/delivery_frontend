d37df054b2b8e0a389a4f5a5b96a47c793dc5049
flexwappa <flexwappa@gmail.com>
2022-08-10 03:01:05 -0400

    create settingsSlice

---
+++ src/store/reducers/settings/settings.slice.ts
@@ -0,0 +1,35 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {UserRoleEnum} from "../backend/backend.api.types";
+
+type SettingsSliceStateType = {
+    role: UserRoleEnum | '';
+    phone: string | undefined;
+    name: string | undefined;
+}
+
+const initialState: SettingsSliceStateType = {
+    role: (localStorage.getItem('role') || '') as (UserRoleEnum | ''),
+    phone: undefined,
+    name: undefined,
+}
+
+export const settingsSlice = createSlice({
+    name: 'settingsSlice',
+    initialState,
+    reducers: {
+        setField(state, {payload: {field, value}}: PayloadAction<{field: keyof typeof initialState | string, value: any}>) {
+            if(field === 'role') {
+                localStorage.setItem(field, value);
+            }
+            // @ts-ignore
+            state[field] = value;
+        },
+        resetState(state, payload) {
+            state = initialState;
+        }
+    }
+});
+
+export const settingsSliceActions = settingsSlice.actions;
+export const settingsReducer = settingsSlice.reducer;
+
