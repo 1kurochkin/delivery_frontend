b449aa922685d420748d670b8df4fe518b0df120
flexwappa <flexwappa@gmail.com>
2022-08-10 03:17:25 -0400

    update tools/validation

--- src/tools/validation.ts
+++ src/tools/validation.ts
@@ -2,6 +2,11 @@ export const validation = (field: string, value: any) => {
     let result = false;
     switch (field) {
         case 'phone': result = true; break;
+        case 'name': result = true; break;
+        case 'lastName': result = true; break;
+        case 'birth': result = true; break;
+        case 'code': result = true; break;
+        case 'role': result = true; break;
         default: result = false
     }
     return {field, isValid: result};
