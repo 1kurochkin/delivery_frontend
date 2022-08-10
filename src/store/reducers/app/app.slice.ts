d7c225d144f50d9eb0377b2f67789d49f596b5cb
flexwappa <flexwappa@gmail.com>
2022-08-10 03:17:29 -0400

    create app slice

---
+++ src/store/reducers/app/app.slice.ts
@@ -0,0 +1,27 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
+
+type AppSliceStateType = {
+    auth: boolean
+}
+
+const initialState: AppSliceStateType = {
+    auth: false
+}
+
+export const appSlice = createSlice({
+    name: 'appSlice',
+    initialState,
+    reducers: {
+        setCookies(state, {payload: {name, value}}: PayloadAction<{name: string, value: any}>) {
+            Cookies.set(name, value);
+        },
+        setAuth(state, {payload}: PayloadAction<boolean>) {
+            state.auth = payload
+        }
+    }
+});
+
+export const appSliceActions = appSlice.actions;
+export const appReducer = appSlice.reducer;
+
