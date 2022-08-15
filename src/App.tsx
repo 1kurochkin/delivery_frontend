33ba64fab8ebd524ff5ce184fd9482d46cc46b21
flexwappa <flexwappa@gmail.com>
2022-08-14 22:18:31 -0400

    add fetch user info logic for app component

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ import {Content} from "antd/es/layout/layout";
 import {ProtectedRoute} from "./components/protectedRoute/protected.route.component";
 import {NotFoundPage} from "./pages/404/404.page";
 import {NotAuthorizedPage} from "./pages/403/403.page";
+import {usePrevious} from "./hooks/usePrevious";
+import Cookies from "js-cookie";
 
 
 function App() {
@@ -29,8 +31,8 @@ function App() {
     // });
     const auth = useAppSelector(({app}) => app.auth)
     const userRole = useAppSelector(({settings}) => settings.role)
-    const prevAuthState = useAppSelector(({app}) => app.auth)
-    const {setField} = useActions();
+    const prevAuthState = usePrevious(auth)
+    const {setSettingsField} = useActions();
 
     const [
         fetchGetCourierInfo,
@@ -40,36 +42,34 @@ function App() {
     const [
         fetchGetCustomerInfo,
         {error: error2, isFetching: fetchingGetCustomerInfo, data: getCustomerInfoData}
-    ] = useLazyGetCustomerInfoQuery(undefined);
+    ] = useLazyGetCustomerInfoQuery();
     const {data: {message: errorGetCustomerInfo = undefined} = {}} = error2 as any || {};
+
+    // useEffect(() => {Cookies.set('sid', '2bb974bc-f4b1-45cd-91cd-5cc14ab377d6')}, [])
 
     useEffect(() => {
         if (!prevAuthState && auth) {
             userRole === UserRoleEnum.Courier ?
-                fetchGetCourierInfo(undefined) :
-                fetchGetCustomerInfo(undefined)
+                fetchGetCourierInfo() :
+                fetchGetCustomerInfo()
         }
     }, [auth])
 
     useEffect(() => {
         if (getCourierInfoData || getCustomerInfoData) {
             const userData = (getCourierInfoData || getCustomerInfoData)
+            console.log(userData)
             for (const field in userData) {
                 // @ts-ignore
-                setField({field, value: userData[field]})
+                setSettingsField({field, value: userData[field]})
             }
         }
     }, [getCourierInfoData, getCustomerInfoData])
 
     //--------CATCH-ERRORS------//
-    // useEffect(() => {
     if (errorGetCourierInfo || errorGetCustomerInfo) {
         notification.error({message: errorGetCourierInfo || errorGetCustomerInfo});
     }
-    // else if (errorLoadGoogleMaps) {
-    //     notification.error({message: 'Error loading maps!'});
-    // }
-    // }, [errorGetCourierInfo, errorGetCustomerInfo])
     //-------------------------//
 
     return (
@@ -88,7 +88,7 @@ function App() {
                                 <Route path={ROUTES.MAIN_PAGE} element={<MainPage/>}/>
                                 <Route path={ROUTES["404"]} element={<NotFoundPage/>}/>
                                 <Route path={ROUTES["403"]} element={<NotAuthorizedPage/>}/>
-                                <Route path="*" element={<Navigate to={ROUTES["404"]}/>}/>
+                                {/*<Route path="*" element={<Navigate to={ROUTES["404"]}/>}/>*/}
                                 <Route path={ROUTES.ORDER.LIST_PAGE}
                                     element={
                                         <ProtectedRoute auth={auth}>
@@ -96,7 +96,7 @@ function App() {
                                         </ProtectedRoute>
                                     }
                                 />
-                                <Route path={ROUTES.ORDER.UPDATE_PAGE}
+                                <Route path={ROUTES.ORDER.UPDATE_PAGE.PATH + ROUTES.ORDER.UPDATE_PAGE.PARAMS}
                                     element={
                                         <ProtectedRoute auth={auth}>
                                             <CreatePage/>
